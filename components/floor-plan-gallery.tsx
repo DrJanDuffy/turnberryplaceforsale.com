2852bb7373bbf9779b4534bebd223665dde8cd92
genekellyboyle <DrDuffy@bhhsnv.com>
2026-01-14 01:51:40 -0800

    Fix useCallback import in floor plan gallery

--- components/floor-plan-gallery.tsx
+++ components/floor-plan-gallery.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import Image from 'next/image'
 import { X, ZoomIn } from 'lucide-react'
 
